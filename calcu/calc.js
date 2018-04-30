36007d0b4fc2cac403d19be131a523845284c5c4
unknown <josmorenoisler@gmail.com>
2018-04-30 16:01:27 -0300

    hola pianola

---
+++ calcu/calc.js
@@ -0,0 +1,20 @@
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+
+var form = document.getElementById('xIsWhatPercentOfy');
+form.addEventListener('submit',function(event){
+    if(!numField1.value || !numField2.value)
+        alert("Pone numeros, puto");
+        else{
+            var i = parseFloat(numField1.value);
+            var j = parseFloat(numField2.value);;
+            var result = i/j;
+            var percent = result*100;
+
+            resultField.innerText="Result: "+percent+"%";
+            //previene que se resetee la pag
+            event.preventDefault();
+        }
+
+});
